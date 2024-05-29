1b196687b7b620624db061f2410e2e841ee0808a
RDUMENKO <3096490@gscs.ca>
2024-05-29 15:07:03 -0600

    upd

--- SocketMessagingClient/Program.cs
+++ SocketMessagingClient/Program.cs
@@ -25,8 +25,18 @@ namespace SocketMessagingClient
             Application.SetCompatibleTextRenderingDefault(false);
             MyClinet = new MessagingClient();
             MyClinet.InitLocalClient();
-            Application.Run(new Login());
+            MyClinet.ConnectionStateUpdated += HandleLogin;
+            Application.Run(new Connection());
             NetworkClient.ClientCreated += ClientCreated;
+        }
+
+        private static void HandleLogin(ConnectionState obj)
+        {
+            if(obj != ConnectionState.Connected)
+            {
+                return;
+            }
+            Application.Run(new Login());
         }
 
         private static void ClientCreated(NetworkClient obj)
