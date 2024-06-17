81b4c8548668a8c71ce68b7ab820fb43e53b269f
BTELNYY <btelnyy@gmail.com>
2024-06-17 14:54:11 -0600

    Revert Client

--- SocketMessagingClient/Login.cs
+++ SocketMessagingClient/Login.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Net.Http.Headers;
@@ -22,7 +23,20 @@ namespace SocketMessagingClient
 
         private void Chat_Load(object sender, EventArgs e)
         {
+#if DEBUG
+            Debugger();
+#endif
+        }
 
+        private void Debugger()
+        {
+            bool LoginSucces = Program.MyClient.ClientLogin("user", "pass");
+            if (LoginSucces)
+            {
+                this.Close();
+                Chat chat = new Chat();
+                chat.ShowDialog();
+            };
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -46,13 +60,19 @@ namespace SocketMessagingClient
             
         }
 
-        private void Login_KeyPress(object sender, KeyPressEventArgs e)
+
+        private void passwordtextbox_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyChar == ((char)Keys.Enter))
-            {
+            if(e.KeyCode == Keys.Return)
                 loginl();
-            }
         }
+
+        private void usernametextbox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return)
+                loginl();
+        }
+
 
         private void loginl()
         {
